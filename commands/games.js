df3d5ecb8de5c92b180fb8fff7a91c06b04d7928
lastmark <par337187@gmail.com>
2026-06-08 16:35:58 +0300

    Update games.js

--- commands/games.js
+++ commands/games.js
@@ -2,19 +2,12 @@ const { SlashCommandBuilder } = require('discord.js');
 const { ICONS } = require('../icons');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('rps')
-        .setDescription('Play Rock, Paper, Scissors')
-        .addStringOption(o => o.setName('choice').setRequired(true)
-            .addChoices({name: 'Rock', value: 'rock'}, {name: 'Paper', value: 'paper'}, {name: 'Scissors', value: 'scissor'})),
-    async execute(interaction) {
-        const choice = interaction.options.getString('choice');
-        const bot = ['rock', 'paper', 'scissor'][Math.floor(Math.random() * 3)];
-        
-        const map = { rock: ICONS.rock, paper: ICONS.paper, scissor: ICONS.scissor };
-        
-        await interaction.reply({
-            content: `${ICONS.bot} **Result:**\nYour choice: ${map[choice]}\nMy choice: ${map[bot]}`
-        });
-    },
+    data: new SlashCommandBuilder().setName('rps').setDescription('Rock Paper Scissors')
+        .addStringOption(o => o.setName('choice').addChoices({name:'Rock', value:'rock'}, {name:'Paper', value:'paper'}, {name:'Scissor', value:'scissor'})),
+    async execute(i) {
+        const c = i.options.getString('choice');
+        const b = ['rock', 'paper', 'scissor'][Math.floor(Math.random() * 3)];
+        const m = { rock: ICONS.rock, paper: ICONS.paper, scissor: ICONS.scissor };
+        await i.reply(`You: ${m[c]} | Me: ${m[b]}`);
+    }
 };
