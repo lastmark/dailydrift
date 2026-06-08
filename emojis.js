ced96279e82e6517c5721c470ff0aadfb475a2ec
lastmark <par337187@gmail.com>
2026-06-08 22:19:05 +0300

    Create emojis.js

---
+++ emojis.js
@@ -0,0 +1,16 @@
+module.exports = {
+  announcement: "<:announcement:1513533499607351356>",
+  bot: "<:bot:1513533291385458708>",
+  coin: "<:coin_flip:1513532556140744856>",
+  error: "<:error:1513532700202631240>",
+  join: "<:memberadd:1513532586998239335>",
+  leave: "<:memberleave:1513532632992845965>",
+  message: "<:message:1513533207037874196>",
+  money: "<:money:1513532519599964270>",
+  paper: "<:paper:1513532786445783151>",
+  rock: "<:rock:1513532823301259446>",
+  scissors: "<:scissor:1513532752669053090>",
+  search: "<:search:1513533580087787530>",
+  settings: "<:setting:1513533096740257993>",
+  user: "<:user:1513533036472307814>"
+};
