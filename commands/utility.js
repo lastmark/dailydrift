1efd41a928070a74587b4e4511b1e66c61d18970
lastmark <par337187@gmail.com>
2026-06-08 19:58:49 +0300

    Delete commands directory

--- commands/utility.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { ICONS } = require('../icons');
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('info').setDescription('Diagnostics'),
-    async execute(i) {
-        await i.reply(`${ICONS.search} Server: ${i.guild.name} | Members: ${i.guild.memberCount}`);
-    }
-};
