5e529c9bb5b9f1b4eb2895409d417cab69b9c569
lastmark <par337187@gmail.com>
2026-06-08 16:34:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { ICONS } = require('./icons');
 const fs = require('fs');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const { ICONS } = require('./icons');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 client.commands = new Collection();
 
-// Command loader
 fs.readdirSync('./commands').forEach(file => {
     const cmd = require(`./commands/${file}`);
     client.commands.set(cmd.data.name, cmd);
 });
 
-client.on('messageCreate', async msg => {
-    // Basic Anti-link logic
-    if (msg.content.includes('http') && !msg.author.bot) {
-        msg.delete();
-        msg.channel.send(`${ICONS.error} Links are blocked here.`);
-    }
-});
-
 client.on('interactionCreate', async i => {
     if (!i.isChatInputCommand()) return;
-    const cmd = client.commands.get(i.commandName);
-    try { await cmd.execute(i); } catch (e) { i.reply(`${ICONS.error} Error.`); }
+    try { await client.commands.get(i.commandName).execute(i); } 
+    catch (e) { i.reply(`${ICONS.error} Error.`); }
 });
 
 client.login(process.env.token);
