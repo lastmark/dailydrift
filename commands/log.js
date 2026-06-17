f97254b7dad7c33255756e580588591a9310c3a6
lastmark <par337187@gmail.com>
2026-06-18 02:41:46 +0300

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -7,6 +7,7 @@ const {
 } = require("discord.js");
 
 module.exports = {
+  category: "Moderation",
   data: new SlashCommandBuilder()
     .setName("log")
     .setDescription("Audit logging system")
