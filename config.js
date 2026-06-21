c67064a8944c91d6effe4cd91e385aca2016a5b9
lastmark <par337187@gmail.com>
2026-06-21 04:58:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ module.exports = {
   clientId: process.env.CLIENT_ID,
   redisUrl: process.env.REDIS_URL,
   devId: "1303357369622990889",
+  TERMS_VERSION: "1.0",
   ECONOMY: {
     MINIMUM_BALANCE: 0,
     DEFAULT_BALANCE: 1000, // Starting coins for new users
     SHIELD_PRICE: 200,
     DOUBLE_XP_PRICE: 500,
     DAILY_BONUS: 50,
-    MIN_BET: 10,
-    MAX_BET: 1000
+  
   }
 };
